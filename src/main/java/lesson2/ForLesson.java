870824c26ac158f172bc4ebfd6dfb1a804ecdadc
Vitalii Lytvynov <vyacheslavxxxxx@gmail.com>
2023-08-27 15:42:49 +0300

    lesson2 and beer

--- src/main/java/lesson2/ForLesson.java
+++ src/main/java/lesson2/ForLesson.java
@@ -3,8 +3,8 @@ package lesson2;
 public class ForLesson {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {// умова поки цикл працює і крок
-           if(i % 5 == 0){
-               System.out.println(i);
+           if(i % 5 == 0 || i % 31 == 0){
+               System.out.println("ПИВО!" + i);
            }
         }
 //        System.out.println(15 % 3 == 0);//
